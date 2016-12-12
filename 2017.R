0fccba5354751fa217dce4d19698333ee7df7354
Joe Brew <joebrew@gmail.com>
2016-12-12 21:58:23 +0200

    updated

--- 2017.R
+++ 2017.R
@@ -7,16 +7,17 @@ dates <- seq(as.Date('2016-12-12'),
              by = 1)
 dow <- weekdays(dates)
 day_number <- 1:length(dates)
-exercises <- c('pushups',
-               'pullups',
+exercises <- c('pullups',
+               'pushups',
                'hanging_knee_balls',
                'ab_roller',
                'hanging_leg_lifts',
-               'cardio_minutes',
                'dips',
                'crunches',
-               'jumps',
                'dynamic_plank',
+               'plank',
+               'calf_raises',
+               'cardio_minutes',
                'sprints',
                'stairs')
 people <- c('joe', 'ben', 'andrew', 'coloma')
@@ -36,27 +37,27 @@ create_day <- function(person = 'joe',
     if(person == 'joe'){
       df$value[df$exercise == 'pushups'] <- 100
       df$value[df$exercise == 'pullups'] <- 50
-      df$value[df$exercise == 'hanging_knee_balls'] <- 50
+      df$value[df$exercise == 'hanging_knee_balls'] <- 0
       df$value[df$exercise == 'hanging_leg_lifts'] <- 50
       df$value[df$exercise == 'cardio_minutes'] <- 30
       df$value[df$exercise == 'dips'] <- 50
       df$value[df$exercise == 'crunches'] <- 0
-      df$value[df$exercise == 'jumps'] <- 50
       df$value[df$exercise == 'dynamic_plank'] <- 50
       df$value[df$exercise == 'sprints'] <- 5
       df$value[df$exercise == 'stairs'] <- 0
+      df$value[df$exercise == 'calf_raises'] <- 50
+      
       
     } else if(person == 'ben'){
       df$value[df$exercise == 'pushups'] <- 200
       df$value[df$exercise == 'pullups'] <- 50
       df$value[df$exercise == 'hanging_knee_balls'] <- 0
       df$value[df$exercise == 'ab_roller'] <- 50
-      df$value[df$exercise == 'hanging_leg_lifts'] <- 50
+      df$value[df$exercise == 'hanging_leg_lifts'] <- 0
       df$value[df$exercise == 'cardio_minutes'] <- 30
       df$value[df$exercise == 'dips'] <- 0
       df$value[df$exercise == 'crunches'] <- 200
-      df$value[df$exercise == 'jumps'] <- 0
-      df$value[df$exercise == 'dynamic_plank'] <- 50
+      df$value[df$exercise == 'plank'] <- 5
       df$value[df$exercise == 'sprints'] <- 0
       df$value[df$exercise == 'stairs'] <- 20
       
@@ -68,7 +69,6 @@ create_day <- function(person = 'joe',
       df$value[df$exercise == 'cardio_minutes'] <- 30
       df$value[df$exercise == 'dips'] <- 50
       df$value[df$exercise == 'crunches'] <- 0
-      df$value[df$exercise == 'jumps'] <- 50
       df$value[df$exercise == 'dynamic_plank'] <- 50
       df$value[df$exercise == 'sprints'] <- 5
       df$value[df$exercise == 'stairs'] <- 0
@@ -80,7 +80,6 @@ create_day <- function(person = 'joe',
       df$value[df$exercise == 'cardio_minutes'] <- 30
       df$value[df$exercise == 'dips'] <- 50
       df$value[df$exercise == 'crunches'] <- 0
-      df$value[df$exercise == 'jumps'] <- 50
       df$value[df$exercise == 'dynamic_plank'] <- 50
       df$value[df$exercise == 'sprints'] <- 5
       df$value[df$exercise == 'stairs'] <- 0
@@ -123,4 +122,15 @@ final <-
          !is.na(value))
 
 final$done <- ''
+final$day_number <- as.numeric(final$date - min(final$date)) + 1
+final <- 
+  final %>%
+  dplyr::select(date, 
+                dow,
+                day_number,
+                person, 
+                exercise,
+                value, 
+                done)
 readr::write_csv(final, '~/Desktop/final.csv')
+
